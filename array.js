3843883a1eea04e6d19336ed634f80ed59dd8912
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-16 10:51:49 +0600

    explore array.includes/indexOf/concat/isArray

---
+++ array.js
@@ -0,0 +1,17 @@
+//* check the array type using Array.isArray()
+const arr = [10, 20, 30, 40, 50, 60]
+// const string = 'takbir';
+// console.log(Array.isArray(arr)) //? returns true
+// console.log(Array.isArray(string)) //? returns false
+
+//* array.includes() method
+// console.log(arr.includes(25)) //? returns false
+// console.log(arr.includes(50)) //? returns true
+
+//* array.indexOf() method
+// console.log(arr.indexOf(60)) //? returns the position of array elements
+// console.log(arr.indexOf(70)) //? returns -1
+
+//* array.concat() method
+const newArr = [99, 98, 97, 96];
+// console.log(arr.concat(newArr))
