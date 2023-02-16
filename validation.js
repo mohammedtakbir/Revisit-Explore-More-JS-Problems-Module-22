1426ff32bca5ac41a5900f286a598f87e43bd247
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-16 17:29:45 +0600

    explore datatype validation

---
+++ validation.js
@@ -0,0 +1,11 @@
+function add(num1, num2) {
+    // if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    //     console.log('please enter valid number')
+    // } else {
+    //     console.log(num1 + num2)
+    // }
+    if(Number.isNaN(num1)){
+        console.log('g')
+    }
+}
+add('efwafe', '35')
